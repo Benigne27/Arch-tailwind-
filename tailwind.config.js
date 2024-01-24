d787637e7c3dd41c483411291243420aaba3e25d
Benigne27 <benigneblessed@gmail.com>
2024-01-24 17:01:04 +0200

    tailwind arch

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./HTML/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        'mansion': "url('/Images/main architect.jpg')",
+        'house1': "url('/Images/house1.jpg')",
+        'house2': "url('/Images/house2.jpg')",
+        'house3': "url('/Images/house3.jpg')",
+        'house4': "url('/Images/house4.jpg')",
+        'house5': "url('/Images/house5.jpg')",
+        'house6': "url('/Images/house6.jpg')",
+        'house7': "url('/Images/house7.jpg')",
+        'house8': "url('/Images/house8.jpg')",
+        'map': "url('/Images/map.jpg')",
+        'profile1': "url('/Images/profile1.jpg')",
+        'profile2': "url('/Images/profile2.jpg')",
+        'profile3': "url('/Images/profile3.jpg')",
+        'profile4': "url('/Images/profile4.jpg')",
+
+      }
+    },
+  },
+  plugins: [],
+}
